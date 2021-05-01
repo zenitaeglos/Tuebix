a7418aea1868bcdd091acd223e6df55056b0f120
zenitaeglos <aegloszenit@protonmail.com>
2021-05-02 00:41:17 +0200

    add cancel action for search bar and lupe

--- TuebixApp/TuebixApp/Views/Elements/SearchBar.swift
+++ TuebixApp/TuebixApp/Views/Elements/SearchBar.swift
@@ -17,6 +17,24 @@ struct SearchBar: View {
                 .padding(.horizontal, 25)
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
                 .padding(.horizontal, 10)
                 .onTapGesture {
                     self.isEditing = true
@@ -25,6 +43,8 @@ struct SearchBar: View {
                 Button(action: {
                     self.isEditing = false
                     self.text = ""
+                    // Dismis the keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
                 }) {
                     Text("Cancel")
                 }
